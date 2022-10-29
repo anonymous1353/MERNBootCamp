f02e862ec7bcb86b7f2d0ab49d5ab7aecf80f247
Hammad Abbasi <abchammad@gamil.com>
2022-10-29 15:32:16 +0500

    chnges made in condition

---
+++ classwork/29-10-22/task.js
@@ -0,0 +1,150 @@
+employee=[{   
+    emp_id:"hammad",
+    emp_name :20,
+    emp_company :"physics",
+    emp_company_address:
+    {
+        com_city :"rwp",
+        com_country: "pak"
+    },  
+    emp_address:{
+        emp_city :"isb",
+        emp_country :"pak"
+    },
+    emp_sal :"50000",
+    islam : 0
+},
+{   
+    emp_id:"zohaib",
+    emp_name :20,
+    emp_company :"physics",
+    emp_company_address:
+    {
+        com_city :"rwp",
+        com_country: "pak"
+    },  
+    emp_address:{
+        emp_city :"isb",
+        emp_country :"pak"
+    },
+    emp_sal :"60000",
+    islam : 1
+},
+{   
+    emp_id:"hamza",
+    emp_name :20,
+    emp_company :"physics",
+    emp_company_address:
+    {
+        com_city :"rwp",
+        com_country: "pak"
+    },  
+    emp_address:{
+        emp_city :"isb",
+        emp_country :"pak"
+    },
+    emp_sal :"30000",
+    islam : 0
+},
+{   
+    emp_id:"zeesahn",
+    emp_name :20,
+    emp_company :"physics",
+    emp_company_address:
+    {
+        com_city :"rwp",
+        com_country: "pak"
+    },  
+    emp_address:{
+        emp_city :"isb",
+        emp_country :"pak"
+    },
+    emp_sal :"90000",
+    islam : 1
+},
+{   
+    emp_id:"furqan",
+    emp_name :20,
+    emp_company :"physics",
+    emp_company_address:
+    {
+        com_city :"rwp",
+        com_country: "pak"
+    },  
+    emp_address:{
+        emp_city :"isb",
+        emp_country :"pak"
+    },
+    emp_sal :"40000",
+    islam : 1
+},{   
+    emp_id:"moiz",
+    emp_name :20,
+    emp_company :"physics",
+    emp_company_address:
+    {
+        com_city :"rwp",
+        com_country: "pak"
+    },  
+    emp_address:{
+        emp_city :"isb",
+        emp_country :"pak"
+    },
+    emp_sal :"80000",islam : 0
+},
+{   
+    emp_id:"awais",
+    emp_name :20,
+    emp_company :"physics",
+    emp_company_address:
+    {
+        com_city :"rwp",
+        com_country: "pak"
+    },  
+    emp_address:{
+        emp_city :"isb",
+        emp_country :"pak"
+    },
+    emp_sal :"70000",
+    islam : 0
+},
+{   
+    emp_id:"raza",
+    emp_name :20,
+    emp_company :"physics",
+    emp_company_address:
+    {
+        com_city :"rwp",
+        com_country: "pak"
+    },  
+    emp_address:{
+        emp_city :"isb",
+        emp_country :"pak"
+    },
+    emp_sal :"10000",
+    islam : 1
+},
+{   
+    emp_id:"ahmar",
+    emp_name :20,
+    emp_company :"physics",
+    emp_company_address:
+    {
+        com_city :"rwp",
+        com_country: "pak"
+    },  
+    emp_address:{
+        emp_city :"isb",
+        emp_country :"pak"
+    },
+    emp_sal :"25000",
+    islam : 0
+}
+]
+
+//console.log(employee.emp_company_address.com_city)
+//.log(employee.emp_address.emp_city)
+
+let a=employee.filter((ele)=>(ele.emp_sal<=40000))
+console.log(a);
+
